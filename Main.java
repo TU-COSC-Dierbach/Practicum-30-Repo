6d3ff1d5b4a918747b820e31cd2df2f34d4bd45c
Charles Dierbach <cdierbach@towson.edu>
2024-04-25 12:06:54 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ class Main {
   public static void main(String[] args) {
     
     // Create a new, empty linked list
-    nums_list = new nums_list;
+    IntList nums_list = new IntList();
 
     // Test 1
     System.out.println("Expected output: * empty list *");
@@ -14,7 +14,7 @@ class Main {
     nums_list.append(10);
 
     // Test 2
-    System.out.println("Expected output: " + 10);
+    System.out.println("Expected output: 10");
     nums_list.displayList();
     System.out.println();
 
@@ -23,7 +23,7 @@ class Main {
     nums_list.append(30);
 
     // Test 3
-    System.out.println("Expected output: " + 10 20 30);
+    System.out.println("Expected output: 10 20 30");
     num_list.displayList();
   }
 
